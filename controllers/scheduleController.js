241ac15b0b0351711f8d2045dd70eaaa9de4f8cc
CommNs <chris20twelve@yahoo.com>
2025-05-13 12:30:53 -0700

    fixed the post form tags

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,7 +1,8 @@
 const User = require('../models/User');
+const Event = require('../models/Event');
 
 exports.postEvent = (req, res, next) => {
-  
+  res.sendStatus(200);
 };
 
 exports.getEvents = (req, res) => {
